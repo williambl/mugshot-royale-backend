34f0e446e0c6d299bbfc41ddf851aded5b760945
William Bradford Larcombe <william@williambl.com>
2018-07-19 23:43:42 +0100

    Added a login

--- mugshot.py
+++ mugshot.py
@@ -1,6 +1,18 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, make_response
+from player import Player
 
 app = Flask(__name__, static_url_path='')
+players = []
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "GET":
+        return send_from_directory("game", "login.html")
+    elif request.method == "POST":
+        players.append(Player(request.form.get("name"), request.remote_addr))
+        response = make_response("done!")
+        response.set_cookie("id", str(len(players)-1))
+        return response
 
 @app.route("/game/<path:path>")
 def send_page(path):
