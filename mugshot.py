e9ea543c223474f4ef07bc2cc9cc5e8c980e77ef
William Bradford Larcombe <william@williambl.com>
2018-07-19 22:50:36 +0100

    Made game server functionall, serving main page and receiving POSTs

--- mugshot.py
+++ mugshot.py
@@ -1,6 +1,12 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, send_from_directory
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__, static_url_path='')
+
+@app.route("/game/<path:path>")
+def send_page(path):
+    return send_from_directory("game", path)
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    print(request.args.get("player"))
+    return request.args.get("player")
