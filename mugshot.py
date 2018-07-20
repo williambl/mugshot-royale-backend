05910e1d60c00b297b426d98adf21bbea29a9837
William Bradford Larcombe <william@williambl.com>
2018-07-20 15:36:01 +0100

    Added websockets Doesn't quite work yet

--- mugshot.py
+++ mugshot.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, send_from_directory, make_response, redirect
-from player import *
 import json
+from flask_socketio import SocketIO, emit
+from player import *
 
 app = Flask(__name__, static_url_path='')
+socketio = SocketIO(app)
 players = []
 
 @app.route("/login", methods=["POST", "GET"])
@@ -11,6 +13,7 @@ def login():
         return send_from_directory("game", "login.html")
     elif request.method == "POST":
         players.append(Player(request.form["name"], request.remote_addr))
+        socketio.emit("player-joined", {"name": request.form["name"]})
         response = redirect("/")
         response.set_cookie("id", str(len(players)-1))
         return response
@@ -37,6 +40,7 @@ def upload():
 
     for player in players:
         if player.name == request.args.get("player"):
+            socketio.emit("player-eliminated", {"name": player.name})
             player.isAlive = False
 
     return request.args.get("player")
@@ -44,3 +48,14 @@ def upload():
 @app.route("/players")
 def send_player_list():
     return json.dumps(players, cls=PlayerEncoder)
+
+@socketio.on('connect', namespace='/websocket')
+def websocket_connect():
+    print("connected!")
+
+@socketio.on('disconnect', namespace='/websocket')
+def websocket_disconnect():
+    print('Client disconnected')
+
+if __name__ == "__main__":
+    socketio.run(app)
