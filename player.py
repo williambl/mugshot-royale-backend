3ce2926df44e398029a8febfe2271dfd9a0df368
williambl <william@williambl.com>
2018-07-22 12:31:59 +0100

    Added team and admin variables to Player

--- player.py
+++ player.py
@@ -3,13 +3,15 @@ from json import JSONEncoder
 
 class Player:
 
-    def __init__(self, name: str, addr: str):
+    def __init__(self, name: str, addr: str, isAdmin: bool, team: str):
         self.name = name
         self.addr = addr
         self.isAlive = True
+        self.isAdmin = isAdmin
+        self.team = team
 
 class PlayerEncoder(JSONEncoder):
     def default(self, obj):
         if isinstance(obj, Player):
-            return {"name": obj.name, "addr": obj.addr, "isAlive": obj.isAlive}
+            return {"name": obj.name, "addr": obj.addr, "isAlive": obj.isAlive, "isAdmin": obj.isAdmin, "team": obj.team}
         return json.JSONEncoder.default(self, obj)
